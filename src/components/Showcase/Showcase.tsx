2d3324ddca98a5aca4851a510d742ddcf80ec54b
Gabriel23Sp <astl5526@gmail.com>
2023-11-02 16:32:02 -0500

    commit update web

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -5,13 +5,13 @@ const Showcase = () => {
 <header className="showcase-principal w-4/5 m-auto h-[550px] pb-[50px] mb-[40px]" id='principal'>
 <div className="content">
   <div className='upper-message z-10 m-3'>
-  <div className='justify-end pb-[50px] mb-[20px] bg-black opacity-80 w-[500px] h-auto z-0 
+  <div className='justify-end  bg-black opacity-90 w-[500px] h-auto z-0 
   max-md:w-[200px]
   max-md:m-10
 '>
   <div className='p-2'>
-    <h2 className='m-2'>Big News Today</h2>
-        <p className='m-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi temporibus tempora accusantium? Assumenda, a aperiam consequatur rem sed porro doloribus?
+    <h2 className='m-2 text-7xl font-bold max-md:text-3xl'>INDUSTRIAL <br />TECH S.A.C. </h2>
+        <p className='m-2 text-3xl font-bold max-md:text-sm'>Soluciones Tecnólogicas
         </p>
     </div>
   </div>
